2cb63fd0dff64bd7940602bed7f8a1004c269e4c
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:32:10 +0200

    Copy src/components/index.ts from ReLaunch

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+// Composants génériques et utilitaires
+export * from './common';
+export * from './layout';
+
+// Modules par domaine fonctionnel (exports regroupés)
+export * as Prospects from './prospects';
+export * as Dashboard from './dashboard';
+export * as Email from './emails';
+export * as History from './history';
+export * as Settings from './settings'; 
