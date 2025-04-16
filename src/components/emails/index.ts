8c4d97c825f2f68b95ddf521b793eab237fe339d
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:32:05 +0200

    Copy src/components/emails/index.ts from ReLaunch

---
+++ src/components/emails/index.ts
@@ -0,0 +1,5 @@
+export { default as TemplatePreview } from './TemplatePreview';
+export { default as EmailPreview } from './EmailPreview';
+export { default as TemplateCard } from './TemplateCard';
+export { default as TemplateForm } from './TemplateForm';
+export { default as TemplateVariables } from './TemplateVariables'; 
