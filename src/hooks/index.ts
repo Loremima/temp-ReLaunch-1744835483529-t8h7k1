1d9134f66f58b69e1028de90eb1b2ed63ddbdb85
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:32:43 +0200

    Copy src/hooks/index.ts from ReLaunch

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useProspects } from './useProspects';
+export { useHistory } from './useHistory';
+export { useSettings } from './useSettings';
+export { useDashboard } from './useDashboard';
+export { useTemplates } from './useTemplates';
+export { useEmails } from './useEmails';
+export { useUser } from './useUser';
+export { useSubscription } from './useSubscription'; 
