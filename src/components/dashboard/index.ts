e7e6fd729a4f5f5e03c46b961f47c85a97d17658
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:31:55 +0200

    Copy src/components/dashboard/index.ts from ReLaunch

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,5 @@
+export { default as DashboardHeader } from './DashboardHeader';
+export { default as UpcomingFollowups } from './UpcomingFollowups';
+export { default as ConversionRates } from './ConversionRates';
+export { default as ActivityChart } from './ActivityChart';
+export { default as StatCard } from './StatCard'; 
