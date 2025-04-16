23fa8fee0a3ed895588fc1137dd98f802ba11f18
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:32:38 +0200

    Copy src/components/settings/index.ts from ReLaunch

---
+++ src/components/settings/index.ts
@@ -0,0 +1,7 @@
+export { default as SettingsNav } from './SettingsNav';
+export { default as SettingsForm } from './SettingsForm';
+export { default as SettingsCard } from './SettingsCard';
+export { default as SettingsButton } from './SettingsButton';
+export { default as ThemeSelector } from './ThemeSelector';
+export { default as LanguageSelector } from './LanguageSelector';
+export { default as DateTimeSettings } from './DateTimeSettings'; 
