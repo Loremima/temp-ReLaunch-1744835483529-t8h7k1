3ecb02e4d175bc06036cd930adb14caca96ccca2
Lorenzo <87819840+Loremima@users.noreply.github.com>
2025-04-16 22:32:24 +0200

    Copy src/components/prospects/index.ts from ReLaunch

---
+++ src/components/prospects/index.ts
@@ -0,0 +1,6 @@
+export { default as AddProspectWizard } from './AddProspectWizard';
+export { default as ProspectForm } from './ProspectForm';
+export { default as ProspectTable } from './ProspectTable';
+export { default as ProspectStatusBadge } from './ProspectStatusBadge';
+export { default as ProspectFilters } from './ProspectFilters';
+export { default as CSVUploadModal } from './CSVUploadModal'; 
